cf0b94eed6f4aebfdf2706011084284d537a8979
agustinhurquiza <agustin.h.urquiza@gmail.com>
2019-02-12 16:01:10 -0300

    Selective Search
    Se agrego una implementacion de selective search

---
+++ SegmentationSelectiveSearch/demo.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from skimage import data
+from imageio import imread
+from datetime import datetime
+
+from selective_search import selective_search
+
+NAME = "image1.jpg"
+
+img = imread(NAME)
+t1 = datetime.now()
+segment_mask, R = selective_search(img, scale=600, sim_threshold=0.77)
+t2 = datetime.now()
+print('Time:', t2-t1)
+
+
+fig = plt.figure()
+ax1 = plt.subplot(121)
+plt.imshow(img)
+
+for r in R:
+    rect = Rectangle((r['x_min'], r['y_min']), r['width'], r['height'],
+                     fill=False, color='red', linewidth=1.5)
+    ax1.add_patch(rect)
+
+ax2 = plt.subplot(122)
+plt.imshow(segment_mask)
+plt.show()
