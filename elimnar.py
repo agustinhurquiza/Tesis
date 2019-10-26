a6e4ede33dc6abb813754260c5aa1201b842f699
agustinhurquiza <agustin.h.urquiza@gmail.com>
2019-10-26 14:32:17 -0300

    Sacar shape

--- elimnar.py
+++ elimnar.py
@@ -29,9 +29,6 @@ def parser():
     # Argumentos de entrada permitidos.
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d', '--dirdata', type=str, required=True,
-                        help="""Directorio donde se encuentan los features.""")
-
     parser.add_argument('-w', '--fword', type=str, required=True,
                         help="""Archivo donde se encuentran las words embeding.""")
 
@@ -57,40 +54,43 @@ def main():
     words = json.load(open(FILEWORD))
     words = {k:v for k,v in words.items() if k in seen}
 
+    for DIRDATA in set([item for item in glob('Data/*')])
     # Carga la data pre-procesadas.
-    X = np.empty((0, INSIZE))
-    Y = np.empty((0, OUTSIZE))
-    for file in set([item[:-len('-_.mat')] for item in glob(DIRDATA+'/*')]):
-        X = np.concatenate((X, load(file + '-X.mat')), axis=0)
-        Y = np.concatenate((Y, load(file + '-Y.mat')), axis=0)
+        X = np.empty((0, INSIZE))
+        Y = np.empty((0, OUTSIZE))
+        for file in set([item[:-len('-_.mat')] for item in glob(DIRDATA+'/*')]):
+            print(DIRDATA, load(file + '-X.mat').shape )
+            break
+            X = np.concatenate((X, load(file + '-X.mat')), axis=0)
+            Y = np.concatenate((Y, load(file + '-Y.mat')), axis=0)
 
-    resultado = {}
-    for k, v in words.items():
-        items = [i for i, y in enumerate(Y) if np.array_equal(y, v)]
-        resultado[k] = np.array(sample(X[items].tolist(), 1000))
+        resultado = {}
+        for k, v in words.items():
+            items = [i for i, y in enumerate(Y) if np.array_equal(y, v)]
+            resultado[k] = np.array(sample(X[items].tolist(), 1000))
 
-    print("Termino de preprocesamiento................")
-    idem = []
-    for k,v in resultado.items():
-        r = 0
-        for _ in range(1000):
-            x1,x2 = sample(list(v), 2)
-            r += x1.dot(x2)
-        idem.append((k,r/1000.0))
+        print("Termino de preprocesamiento................")
+        idem = []
+        for k,v in resultado.items():
+            r = 0
+            for _ in range(1000):
+                x1,x2 = sample(list(v), 2)
+                r += x1.dot(x2)
+            idem.append((k,r/1000.0))
 
-    dist = []
-    for (k1, k2) in itertools.combinations(resultado, 2):
-        cl1 = resultado[k1]
-        cl2 = resultado[k2]
-        r = 0
-        for _ in range(1000):
-            x1 = sample(list(cl1), 1)[0]
-            x2 = sample(list(cl2), 1)[0]
-            r += x1.dot(x2)
-        dist.append(((k1, k2), r/1000.0))
-    print(idem)
-    print('--------------------------------------')
-    print(dist)
+        dist = []
+        for (k1, k2) in itertools.combinations(resultado, 2):
+            cl1 = resultado[k1]
+            cl2 = resultado[k2]
+            r = 0
+            for _ in range(1000):032
+                x1 = sample(list(cl1), 1)[0]
+                x2 = sample(list(cl2), 1)[0]
+                r += x1.dot(x2)
+            dist.append(((k1, k2), r/1000.0))
+        print(idem)
+        print('--------------------------------------')
+        print(dist)
 
 
 if __name__ == "__main__":
